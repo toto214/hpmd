b02f2844494d9fd7601f24c3149306ef475944d6
toto214 <t.fallet@gmail.com>
2019-10-25 15:51:52 +0700

    update news

--- admin/index.php
+++ admin/index.php
@@ -62,6 +62,81 @@ if(isset($_SESSION["USER_ID"])){
                             Welcome
                         </div>
                     </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div>
+                                <p>ทดสอบระบบ อัพเดทข้อมูลของฝ่ายส่งเสริมฯ</p>
+                                 <form action="#" method="POST" autocomplete="off">
+                                <div class="alert alert-danger alert-incorrect d-none" role="alert"></div>
+                                <?php if(isset($_COOKIE["user_id"])){?>
+                                <?php foreach($_COOKIE["user_id"] as $k => $v){?>
+                                <div title="<?php echo $v;?>" style="width:99px;display:inline-block;position:relative;cursor:pointer;" class="text-center card-user-cookie card-user-<?php echo $k;?> formlogin" data-key="<?php echo $k;?>" data-username="<?php echo $v;?>">
+                                <button type="button" class="close" aria-label="Close" style="position: absolute; right: 14px;" data-toggle="modal" data-target="#deleteModal" data-key="<?php echo $k;?>" data-username="<?php echo $v;?>">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                <div class="account_info" data-key="<?php echo $k;?>" data-username="<?php echo $v;?>">
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" alt="" width="75" class="img-fluid rounded-circle img-thumbnail">
+                                    <?php echo "<p>".$v."</p>";?>
+                                </div>
+                                </div>   
+                                <?php } ?>
+                                <?php } ?> 
+
+                                <div title="" style="width:99px;display:inline-block;position:relative;cursor:pointer;" class="text-center  formlogin card-user-choose d-none mx-auto">
+                                <button type="button" class="close" aria-label="Close" style="position: absolute; right: 14px;" data-toggle="modal" data-target="#deleteModal">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                <div class="account_info_choose" >
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" alt="" width="75" class="img-fluid rounded-circle img-thumbnail">
+                                    
+                                </div>
+                                <p style="display:inline-block;margin: 0;"></p> <span class="small notme text-danger">Not Me?</span>
+                                </div>  
+
+                                <div class="form-group username">
+                                    <div class="input-group">
+                                    <input type="text" class="form-control login-input" id="username" name="username" placeholder="Username" value="">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text login-input-group-text" id="btnGroupAddon"><span class="fa fa-user"></span>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <span id="error-username" class="text-danger small"></span>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                    <input type="password" class="form-control login-input" id="password" name="password" placeholder="Password" value="">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text login-input-group-text" id="btnGroupAddon"><span class="fa fa-lock"></span>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <span id="error-password" class="text-danger small"></span>
+                                </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-checkbox my-1 mr-sm-2">
+                                    <input type="checkbox" name="remember" value="1" class="custom-control-input" id="remember">
+                                    <label class="custom-control-label small" for="remember">Remember Me</label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block btn-login">Login</button>
+                                    <button class="btn btn-primary btn-block d-none btn-loading" type="button" disabled>
+                                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                    Login...
+                                    </button>
+                                </div>
+                                <!-- <div class="text-center">
+                                    <a href="#" class="small">forgot password?</a>
+                                </div> -->
+                                <!-- <div class="text-center">
+                                    <a href="register.php" class="small">Register</a>
+                                </div> -->
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
